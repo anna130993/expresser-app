dc489b45cd7e91eb4d4e630c7aa7761e84e3e683
Anna Podsiadlo <annamagdalena.podsiadlo@gmail.com>
2021-04-29 00:14:32 +0200

    Add contact subpage, message display, img display

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('express-handlebars');
+const multer = require('multer');
 
 const app = express();
 app.engine('hbs', hbs());
 app.set('view engine', 'hbs');
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'public/')
+    },
+    filename: function(req, file, cb) {
+        cb(null, file.fieldname)
+    }
+})
+const upload = multer({storage: storage})
 
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.render('index');
@@ -22,6 +33,17 @@ app.get('/about', (req, res) => {
 
 app.get('/contact', (req, res) => {
   res.render('contact');
+});
+
+app.post('/contact/send-message', upload.single('design'), (req, res) => {
+  const { author, sender, title, message } = req.body;
+  const design = req.file ? req.file : null;
+  if(author && sender && title && message && design) {
+    res.render('contact', {isSent: true, file_name: design.originalname});
+  }
+  else {
+    res.render('contact', {isError: true});
+  }
 });
 
 app.get('/info', (req, res) => {
